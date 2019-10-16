d9dd69b13d0f1e98134369fd2ae86fc74d3afdb2
cronwel <noelirias@gmail.com>
2019-10-16 16:59:32 -0500

    10

---
+++ frontend/components/Nav.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
+
+const Nav = () => (
+  <NavStyles>
+    <Link href="/sell">
+      <a>Sell</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
+);
+
+export default Nav;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,5 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
-
 class MyApp extends App {
   render() {
     const { Component } = this.props;
